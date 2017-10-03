34ca619c40489af7cce9592f0737f8d6d6566ce1
Kristen Peck <kpeck71@gmail.com>
2017-10-03 02:38:00 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,6 +18,7 @@ class String
     binding.pry
     #complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
     self.split("." || "?" || "!").count
-    #splits.reject {|strings| strings.empty?}
+    complex_string.split("." || "?" || "!").count
+    #complex_string.reject {|strings| strings.empty?}
   end
 end
