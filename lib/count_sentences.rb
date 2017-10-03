b617f33f487185221b769b49412d085f3d5f304f
Kristen Peck <kpeck71@gmail.com>
2017-10-03 02:35:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     #binding.pry
-    self.split("." || "?" || "!").count
+    self.split("." || "?" || "!").to_s.count
     #splits.reject {|strings| strings.empty?}
   end
 end
